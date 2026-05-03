d1f1cd58225698442fcd05eb70011abc9dd50b44
md-moynul <mdmoynulislam58995@gmail.com>
2026-05-03 14:49:12 +0600

    added animation in hero page

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 const Banner = () => {
   return (
-    <div className="bg-[url('https://i.ibb.co.com/XZffcNK9/image-7378ffaf.png')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl container mx-auto">
+    <div className="bg-[url('https://i.ibb.co.com/XZffcNK9/image-7378ffaf.png')] h-[60vh] w-full bg-cover bg-no-repeat bg-center flex items-center rounded-lg shadow-2xl container mx-auto animate__animated animate__bounceInRight duration-500">
       {/* Overlay */}
       <div className="w-full h-full rounded-lg bg-green-800/50 flex items-center ">
         <div className="max-w-7xl px-6 text-white">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-2xl animate__animated animate__backInLeft">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-2xl animate__animated animate__bounceInLeft delay-600 duration-300">
             The Smartest Way to Book Your Qurbani.
           </h1>
-          <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200">
+          <p className="text-lg md:text-xl mb-6 max-w-xl text-gray-200 animate__animated animate__bounceInUp delay-900">
            QurbaniHat connects you with premium livestock for your Eid-ul-Adha. Select, book, and relax—we handle the rest.
           </p>
 
-          <div className="flex gap-4">
+          <div className="flex gap-4 animate__animated animate__bounceInDown delay-1300">
             <Link href="#Featured">
               <Button variant="primary" className="bg-orange-500">
                 Book Now
